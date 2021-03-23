59d5fe99a9afdf467a408c68089adf40cf29f704
Роман КУдаков <nastuwa9090@mail.ru>
2021-03-23 21:00:19 +0300

    1

---
+++ Практика 6/Практика 6/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_6
+{
+  public partial class Form1 : Form
+  {
+    int[] arr = new int[15];
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      Random rand = new Random();
+      textBox1.Text = "";
+      for(int i = 0; i < arr.Length; i++)
+      {
+        arr[i] = rand.Next(-100, 100);
+        textBox1.Text += "Mas[" + Convert.ToString(i+1) + "] = " + Convert.ToString(arr[i]) + Environment.NewLine;
+      }
+
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      textBox2.Text = "";
+      for (int i = 0; i < arr.Length; i++)
+      {
+        if (arr[i] < 0) arr[i] = 0;
+
+        textBox2.Text += "Mas[" + Convert.ToString(i+1) + "] = " + Convert.ToString(arr[i]) + Environment.NewLine;
+      }
+    }
+  }
+}
