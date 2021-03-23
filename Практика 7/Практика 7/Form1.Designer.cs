fd4429c3d033d5f236dc90aa1b003a59887a9103
Роман КУдаков <nastuwa9090@mail.ru>
2021-03-23 21:41:00 +0300

    1

---
+++ Практика 7/Практика 7/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Практика_7
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.button1 = new System.Windows.Forms.Button();
+      this.dataGridView2 = new System.Windows.Forms.DataGridView();
+      this.label1 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.AllowUserToOrderColumns = true;
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(2, 12);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.Size = new System.Drawing.Size(1199, 245);
+      this.dataGridView1.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.button1.Location = new System.Drawing.Point(2, 263);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Заполнить матрицу";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // dataGridView2
+      // 
+      this.dataGridView2.AllowUserToOrderColumns = true;
+      this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView2.Location = new System.Drawing.Point(2, 292);
+      this.dataGridView2.Name = "dataGridView2";
+      this.dataGridView2.Size = new System.Drawing.Size(1216, 244);
+      this.dataGridView2.TabIndex = 3;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 539);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(59, 13);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Результат";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1287, 681);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.dataGridView2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.dataGridView1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.DataGridView dataGridView1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.DataGridView dataGridView2;
+    private System.Windows.Forms.Label label1;
+  }
+}
+
---
+++ Практика 7/Практика 7/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_7
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      dataGridView1.RowCount = 10;
+      dataGridView1.ColumnCount = 10;
+      dataGridView2.RowCount = dataGridView1.RowCount;
+      dataGridView2.ColumnCount = dataGridView1.ColumnCount;
+      int[,] arr = new int[10, 10];
+      Random rand = new Random();
+      for (int i = 0; i < dataGridView1.RowCount; i++)
+      {
+        for (int j = 0; j < dataGridView1.ColumnCount; j++)
+        {
+          arr[i, j] = rand.Next(-10, 11);
+          dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(arr[i, j]);
+        }
+      }
+      int max = int.MinValue, min = int.MaxValue;
+      int max_index = -1, min_index = -1;
+      for (int i = 0; i < dataGridView1.RowCount; i++)
+      {
+        for (int j = 0; j < dataGridView1.ColumnCount; j++)
+        {
+          if (arr[i, j] < min)
+          {
+            min = arr[i, j];
+            min_index = i;
+          }
+          if (arr[i, j] > max)
+          {
+            max = arr[i, j];
+            max_index = i;
+          }
+        }
+      }
+      int[] arr_buff = new int[dataGridView2.ColumnCount];
+      for (int i = 0; i < dataGridView2.ColumnCount; i++)
+      {
+        arr_buff[i] = arr[min_index, i];
+        arr[min_index, i] = arr[max_index,i];
+        arr[max_index, i] = arr_buff[i];
+      }
+      for (int i = 0; i < dataGridView2.RowCount; i++)
+      {
+        for (int j = 0; j < dataGridView2.ColumnCount; j++)
+        {
+          dataGridView2.Rows[i].Cells[j].Value = Convert.ToString(arr[i, j]);
+        }
+      }
+    }
+  }
+}
